f02c6cba7b0f62eb92fac47e8a1150d57b0dae27
Zaman Mehdi <zamanmehdi110@gmail.com>
2020-07-11 22:50:59 +0500

    app.js setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const feedRouters = require('./routers/feed');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET POST PUT PATCH DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type Authorization');
+    next();
+})
+
+app.use('/feed', feedRouters);
+
+app.listen(8080);

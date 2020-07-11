ec6177e502119aff6475c091e3af2ca1d7d7ff2d
Zaman Mehdi <zamanmehdi110@gmail.com>
2020-07-12 01:02:36 +0500

    post model setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRouters = require('./routers/feed');
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -17,4 +17,14 @@ app.use((req, res, next)=>{
 
 app.use('/feed', feedRouters);
 
-app.listen(8080);
+mongoose
+    .connect(
+        'mongodb+srv://ZamanMehdi:qazwsx74@cluster0.cjl3w.mongodb.net/Post-Feed-Web-App'
+    )
+    .then(result => {
+        app.listen(8080);
+        console.log('Connected');
+    })
+    .catch(error => {
+        console.log(error);
+    })

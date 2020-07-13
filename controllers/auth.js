551bae06f85dbc688e50d2f82e929027a23740b3
Zaman Mehdi <zamanmehdi110@gmail.com>
2020-07-13 11:23:06 +0500

    sign up done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,38 @@
-exports.signUp = (req, res, next) => {
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-}
+exports.signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed.');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt
+        .hash(password, 12)
+        .then(hashedPw => {
+            const user = new User({
+                email,
+                name,
+                password: hashedPw
+            })
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({
+                message: 'User Creted',
+                userId: result._id
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const { body } = require('express-validator');
 
+const User = require('../models/user');
 const authControllers = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/signup', authControllers.signUp);
+router.put(
+    '/signup',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please Enter Valid Email')
+            .custom((value, { req }) => {
+                return User.findOne({ email: value })
+                    .then(userDoc => {
+                        if (userDoc) {
+                            return Promise.reject('E-Mail address already exists!');
+                        };
+                    });
+            })
+            .normalizeEmail(),
+        body('password')
+            .trim()
+            .isLength({
+                min: 5
+            }),
+        body('name')
+            .trim()
+            .not()
+            .isEmpty()
+    ],
+    authControllers.signup
+);
 
 module.exports = router;
